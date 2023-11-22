f851ebbe217d6b13313ecfd44a820b5aa8489bed
devhoya97 <diajbh123@gmail.com>
2023-11-22 15:33:12 +0900

    feat: 게임이 종료되면 안내메시지를 출력하는 기능 구현

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,12 +1,15 @@
 package blackjack;
 
 import blackjack.view.InputView;
+import blackjack.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        OutputView.printGameStart();
         while (true) {
 
             if (!(InputView.getMoreGameOrNot())) {
+                OutputView.printGameOver();
                 return;
             }
         }
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -2,7 +2,15 @@ package blackjack.view;
 
 public class OutputView {
     private static final String NEW_LINE = "\n";
-    public static void printStart() {
+    public static void printGameStart() {
         System.out.println("간단 카드 게임을 시작합니다.");
+    }
+
+    public static void printGameOver() {
+        StringBuilder message = new StringBuilder();
+        message.append("게임을 종료합니다.")
+                .append(NEW_LINE)
+                .append("플레이해주셔서 감사합니다.");
+        System.out.println(message);
     }
 }
