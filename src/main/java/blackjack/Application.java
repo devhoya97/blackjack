b0dd3688945c7c050e7fd0688d03555ad6ed9bbc
devhoya97 <diajbh123@gmail.com>
2023-11-22 15:27:29 +0900

    feat: 입력 값에 따라 게임을 계속할지 결정하는 기능 구현

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -4,7 +4,11 @@ import blackjack.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        boolean isMoreGame = InputView.getMoreGameOrNot();
-        System.out.println(isMoreGame);
+        while (true) {
+
+            if (!(InputView.getMoreGameOrNot())) {
+                return;
+            }
+        }
     }
 }
