6f42bee5dfb292659e0d0e105d4da3bb5f1c07c5
devhoya97 <diajbh123@gmail.com>
2023-11-22 15:25:25 +0900

    feat: 게임을 계속할지 여부를 입력받는 기능 구현

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,4 +1,10 @@
 package blackjack;
 
+import blackjack.view.InputView;
+
 public class Application {
+    public static void main(String[] args) {
+        boolean isMoreGame = InputView.getMoreGameOrNot();
+        System.out.println(isMoreGame);
+    }
 }
---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,19 @@
+package blackjack.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static boolean getMoreGameOrNot() {
+        Scanner console = new Scanner(System.in);
+        while(true) {
+            System.out.print("한 게임 더 하시겠습니까? (Y / N) ");
+            String input = console.nextLine();
+            char yesOrNo = Character.toUpperCase(input.charAt(0));
+            if ((input.length() != 1) || (yesOrNo != 'Y' && yesOrNo != 'N')) {
+                System.out.println("잘못 입력하셨습니다.");
+                continue;
+            }
+            return yesOrNo == 'Y';
+        }
+    }
+}
