bd10315fb7c17aa4b7746071ae850030969d9f5e
devhoya97 <diajbh123@gmail.com>
2023-11-22 16:19:57 +0900

    feat: 카드뭉치의 모든 번호들을 반환하는 기능 구현

--- src/main/java/blackjack/Cards.java
+++ src/main/java/blackjack/Cards.java
@@ -4,10 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cards {
-    private List<Card> cards;
+    private List<Card> cards = new ArrayList<>();
 
-    public Cards(Card card) {
-        cards = new ArrayList<>();
+    public Cards() {
+    }
+
+    public void addCard(Card card) {
         cards.add(card);
+    }
+
+    public int getMatchCount() {
+        return cards.size();
+    }
+
+    public List<Integer> getCardNumbers() {
+        List<Integer> cardNumbers = new ArrayList<>(cards.size());
+        for (int cardIndex = 0; cardIndex < cards.size(); cardIndex++) {
+            Card card = cards.get(cardIndex);
+            cardNumbers.add(card.getNumber());
+        }
+        return cardNumbers;
     }
 }
