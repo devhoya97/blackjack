6efbca3cf689ddc14630f1e4c2335b5a7fb495db
devhoya97 <diajbh123@gmail.com>
2023-11-22 15:30:06 +0900

    feat: 최초 게임을 시작하면 게임의 타이틀을 표시하는 기능 구현

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,4 +1,8 @@
 package blackjack.view;
 
 public class OutputView {
+    private static final String NEW_LINE = "\n";
+    public static void printStart() {
+        System.out.println("간단 카드 게임을 시작합니다.");
+    }
 }
