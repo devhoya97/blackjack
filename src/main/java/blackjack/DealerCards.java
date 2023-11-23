abab7024579a64dfbfeb4ec654ac84c2520b00bc
devhoya97 <diajbh123@gmail.com>
2023-11-23 13:45:37 +0900

    feat: 딜러는 카드 합이 16이하일 때만 카드를 받는 기능 구현

--- src/main/java/blackjack/DealerCards.java
+++ src/main/java/blackjack/DealerCards.java
@@ -1,2 +1,15 @@
-package blackjack;public class DealerCards {
+package blackjack;
+
+public class DealerCards extends Cards{
+    private static final int MAX_CARD_SUM_ALLOWED = 16;
+    public DealerCards() {
+        super();
+    }
+
+    public void addCard() {
+        Card card = new Card();
+        if (sum() <= MAX_CARD_SUM_ALLOWED) {
+            cards.add(card);
+        }
+    }
 }
