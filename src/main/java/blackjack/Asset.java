d4ad59410eb5baef3ece9e308ac5cb0ff781602e
devhoya97 <diajbh123@gmail.com>
2023-11-23 10:12:41 +0900

    feat: 최초 자산 1000원을 지급하는 기능 구현

---
+++ src/main/java/blackjack/Asset.java
@@ -0,0 +1,19 @@
+package blackjack;
+
+public class Asset {
+    private static final int INITIAL_MONEY = 1000;
+    private int money;
+
+    public Asset() {
+        this.money = INITIAL_MONEY;
+    }
+
+    public void increase(int money) {
+
+    }
+
+    public void decrease(int money) {
+
+    }
+
+}
